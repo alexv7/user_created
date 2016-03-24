9dcbd4fc954774315b9d9e0d96253d387598a616
alexv7 <alexvarjabedian7@gmail.com>
2016-03-23 21:58:21 -0400

    added user create method and flash messaging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
@@ -14,5 +15,22 @@ tracking down application errors and interactively debugging your application.
 =end
   end
 
+  def create
+   @user = User.new(user_params)
+   if @user.save
+     flash[:success] = "Welcome to the Sample App!"
+     redirect_to @user
+    #  redirect_to user_url(@user)   #same as above
+   else
+     render 'new'
+   end
+ end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
 end
