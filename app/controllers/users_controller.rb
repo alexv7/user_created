5d024d02464d575fe14524dd230c1e800cb9006e
alexv7 <alexvarjabedian7@gmail.com>
2016-03-28 19:31:56 -0400

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,19 +16,29 @@ tracking down application errors and interactively debugging your application.
   end
 
   def create
-   @user = User.new(user_params)
-   if @user.save
-     log_in @user
-     flash[:success] = "Welcome to the Sample App!"
-     redirect_to @user
-   else
-     render 'new'
-   end
- end
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
- def edit
+  def edit
     @user = User.find(params[:id])
- end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
