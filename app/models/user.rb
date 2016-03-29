07b415ccd019b48dc18ff148261b171d3c0793ca
alexv7 <alexvarjabedian7@gmail.com>
2016-03-29 19:23:54 -0400

    added account activation code to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email   # before_save { self.email = email.downcase }  # we created a method for this now
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
@@ -13,7 +14,7 @@ class User < ActiveRecord::Base
   # passwords. (Because nil passwords now bypass the main presence validation but
   # are still caught by has_secure_password , this also fixes the duplicate error
   # message mentioned in Section 7.3.3.).
-  # Basically what this is doing is that when updating/editing the user attributes, if 
+  # Basically what this is doing is that when updating/editing the user attributes, if
   # the user does not update the password and confirmation fields (leaves it nil)
   # then it will keep its password as the original password that they
   # used when they signed up.
@@ -48,7 +49,24 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  private
+
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 end
+
+
+
+
 
 =begin
 
