d19667c655c1fafe59d25d452f962600a4ad5cd5
alexv7 <alexvarjabedian7@gmail.com>
2016-03-28 20:17:36 -0400

    added friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -48,6 +49,12 @@ tracking down application errors and interactively debugging your application.
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
   private
